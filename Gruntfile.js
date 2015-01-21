71fba5a91346c50fe8b4c30efd41001419302080
Gennady Karev <pendolf666@gmail.com>
2015-01-21 16:26:54 +0600

    Setting up the sass and watch plugins in our GruntFile

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,8 +10,25 @@ module.exports = function(grunt) {
                 app: ['homepage'],
                 assets: ['<%= project.app %>/assets'],
                 css: ['<%= project.assets %>/sass/style.scss']
+            },
+            sass: {
+                dev: {
+                    options: {
+                        style: 'expanded',
+                        compass: false
+                    },
+                    files: {
+                        '<%= project.assets %>/css/style.css':'<%= project.css %>'
+                    }
+                }
+            },
+            watch: {
+                sass: {
+                    files: '<%= project.assets %>/sass/{,*/}*.{scss,sass}',
+                    tasks: ['sass:dev']
+                }
             }
-    });
+    });    
 
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
