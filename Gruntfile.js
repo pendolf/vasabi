d7a2f87399faf94ab9ca3aff4c36a341f118ca49
Gennady Karev <pendolf666@gmail.com>
2015-01-21 16:21:29 +0600

    Add grunt dep file. Init project settings.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,13 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
 
+            pkg: grunt.file.readJSON('package.json'),
+
+            project: {
+                app: ['homepage'],
+                assets: ['<%= project.app %>/assets'],
+                css: ['<%= project.assets %>/sass/style.scss']
+            }
     });
 
     grunt.registerTask('default', [
